b934d1a425aab6e5ae40af56da002a4ce19a2f17
PeterSansan <1657225860@qq.com>
2020-07-21 11:15:46 +0800

    add Cat

---
+++ jvm/src/main/scala/cn.peter.hello/Cat.scala
@@ -0,0 +1,8 @@
+package cn.peter.hello
+import scala.util.Random.nextInt
+class Cat(val name:String,val age:Int) {
+  val id:String = name + nextInt(100).toString
+  def meow(count:Int): Unit = {
+    (0 until count).foreach(i => println(s"$i: meow"))
+  }
+}
