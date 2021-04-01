96b493d3fd0a88831bb500c1d15856b7924c9ac8
PeterSansan <1657225860@qq.com>
2021-04-02 01:39:16 +0800

    test

--- jvm/src/main/scala/cn.peter.hello/Cat.scala
+++ jvm/src/main/scala/cn.peter.hello/Cat.scala
@@ -4,5 +4,6 @@ class Cat(val name:String,val age:Int) {
   val id:String = name + nextInt(100).toString
   def meow(count:Int): Unit = {
     (0 until count).foreach(i => println(s"$i: meow"))
+    println(".......")
   }
 }
